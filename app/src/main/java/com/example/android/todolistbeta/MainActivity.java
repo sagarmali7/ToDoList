8b0a12393238770c8d573340ac0dd1d2afecc1ce
Erick Guzman <erickguzman778@gmail.com>
2018-04-09 11:41:47 -0400

    Added dependency for recycler view Added code to XML file Finished the recycler view setup

--- app/src/main/java/com/example/android/todolistbeta/MainActivity.java
+++ app/src/main/java/com/example/android/todolistbeta/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.android.todolistbeta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataSet;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDataSet = new String[1000];
+
+        mRecyclerView = findViewById(R.id.recycler_view_main);
+
+        //If content does not change, improves performances
+        mRecyclerView.setHasFixedSize(true);
+
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+
+        mAdapter = new MyAdapter(myDataSet);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
