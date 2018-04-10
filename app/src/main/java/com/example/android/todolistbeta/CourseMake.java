a42492fc760752709b9a7a48cf53774756ab2ce7
Erick Guzman <erickguzman778@gmail.com>
2018-04-10 11:55:38 -0400

    Added dependency for recycler view Added code to XML file Finished the recycler view setup Added courses class and as well as hard code Check button Added comments Intent switch works now Back button works now

--- app/src/main/java/com/example/android/todolistbeta/CourseMake.java
+++ app/src/main/java/com/example/android/todolistbeta/CourseMake.java
@@ -1,7 +1,11 @@
 package com.example.android.todolistbeta;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 
 /**
  * Created by kellyzelaya on 4/10/18.
@@ -9,10 +13,11 @@ import android.support.v7.app.AppCompatActivity;
 
 public class CourseMake extends AppCompatActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.course_maker);
     }
+
 
 
 }
