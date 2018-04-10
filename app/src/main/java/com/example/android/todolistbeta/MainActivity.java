40c4854d348f70e5b4bd9b74bbde8bc12fb35eae
sagar <sagarbmali7@gmail.com>
2018-04-09 22:01:29 -0400

    Made the app look better and added more test courses to see how it looked

--- app/src/main/java/com/example/android/todolistbeta/MainActivity.java
+++ app/src/main/java/com/example/android/todolistbeta/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
     private List<Course> courseList = new ArrayList<>();
     private RecyclerView recyclerView;
     private CourseAdapter mCourseAdapter;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,21 +26,34 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(mCourseAdapter);
 
-
         prepareCourseList();
-
-
     }
 
 
     private void prepareCourseList() {
-        Course course1 = new Course("CMSC", "131");
+        Course course1 = new Course("CMSC131", "Number of tasks: 3");
         courseList.add(course1);
 
-        Course course2 = new Course("CMSC", "132");
+        Course course2 = new Course("CMSC132", "Number of tasks: 5");
         courseList.add(course2);
 
+        Course course3 = new Course("ENEE205", "Number of tasks: 2");
+        courseList.add(course3);
 
+        Course course4 = new Course("ENEE222", "Number of tasks: 1");
+        courseList.add(course4);
+
+        Course course5 = new Course("ENEE322", "Number of tasks: 10");
+        courseList.add(course5);
+
+        Course course6 = new Course("ENEE303", "Number of tasks: 0");
+        courseList.add(course6);
+
+        Course course7 = new Course("ENEE307", "Number of tasks: 6");
+        courseList.add(course7);
+
+        Course course8 = new Course("MATH241", "Number of tasks: 0");
+        courseList.add(course8);
 
         mCourseAdapter.notifyDataSetChanged();
     }
