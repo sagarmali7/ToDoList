51303f7512e4ce745662093582aa2ca43f5ee559
Erick Guzman <erickguzman778@gmail.com>
2018-04-12 00:52:45 -0400

    Added dependency for recycler view Added code to XML file Finished the recycler view setup Added courses class and as well as hard code Check button Added comments Intent switch works now Back button works now Added image and added stuff to CourseMaker class Added the calendar to the app

--- app/src/main/java/com/example/android/todolistbeta/CourseMake.java
+++ app/src/main/java/com/example/android/todolistbeta/CourseMake.java
@@ -1,11 +1,15 @@
 package com.example.android.todolistbeta;
 
-import android.content.Intent;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 
 /**
@@ -17,6 +21,34 @@ public class CourseMake extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.course_maker);
+
+        FloatingActionButton caldendarButton = findViewById(R.id.calendar);
+        final Calendar myCalendar  = Calendar.getInstance(); // Returns calendar fields that have been
+        //Initialized with the current date and time
+
+
+
+        //Sets the calendar to the current year, month and day of the month
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker view,int year, int monthOfYear,int dayOfMonth){
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            }
+        };
+
+
+
+        //Once the button is clicked, shows a "dialog" box of the calendar with the
+        //current YEAR, MONTH, DAY OF THE MONTH
+        caldendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(CourseMake.this,date,myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
 
 
